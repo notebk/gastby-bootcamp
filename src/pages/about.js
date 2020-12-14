24a0fc7989926ba6e675fb742f3eafe449766432
Eugenio <eglz@pop-os.localdomain>
2020-12-14 10:44:16 -0600

    using gastby link

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 const AboutPage = () => {
     return (
         <div>
             <h1>About Me</h1>
             <p>I am Eugenio, and I am working on learning Gatsby.js</p>
+            <p>Need a developer? <Link to="/contact">Contact me.</Link></p>
+
         </div>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 const indexPage = () => {
   return (
     <div>
       <h1>Hello.</h1>
       <h2>I'm Andrew, a full-stack developer living in beautiful Philadelphia.</h2>
+      <p>Need a developer? <Link to="/contact">Contact me.</Link></p>
     </div>
   )
 }
