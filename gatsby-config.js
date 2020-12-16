384c1ce537a00af65f3302ae06c5a9d90e851208
22087155+notebk@users.noreply.github.com <22087155+notebk@users.noreply.github.com>
2020-12-16 13:14:29 -0600

    graphql usage

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,10 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: 'Full-Stack Bootcamp!',
+    author: 'Eugenio Gonzalez'
+  },
   plugins: [
     'gatsby-plugin-sass'
   ],
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Link, graphql, useStaticQuery } from 'gatsby'
 
 const Footer = () => {
+    const data = useStaticQuery(graphql`
+        query {
+            site {
+                siteMetadata {
+                    author
+                }
+            }
+        }
+    `)
     return (
         <footer>
-            <p>Created by Eugenio Gonzalez, Copyright 2020</p>
+            <p>Created by {data.site.siteMetadata.author}, Copyright 2020</p>
         </footer>
     )
 }
